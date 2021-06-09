8d4a19834b0077ee08e561148121c720bad87be2
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-06-10 00:59:21 +0530

    Create If-else.py

---
+++ Conditional_Statements/If-else.py
@@ -0,0 +1,27 @@
+a=input("enter the number")
+a=int(a)                            #typecasting
+if(a>10):
+    print("number is greater than 10")
+else:
+    print("number is smaller than or equals to 10 ")
+
+
+age=input("enter the age")
+age=int(age)                        #typecasting
+if(age>=18):
+    print("person can caste their vote")
+else:
+    print("person can't caste their vote")
+
+
+#nested if else
+a=input("enter a number between 1-10")
+a=int(a)
+if(a<5):
+    print("number is smaller then 5")
+elif(a<10):
+    print("number is less then 10")
+else:
+    print("number is out of range")
+
+
