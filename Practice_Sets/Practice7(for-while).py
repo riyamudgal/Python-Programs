c3f0a97ec61491f7c844ea44b7449f171e0cb657
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-06-14 00:07:32 +0530

    Update Practice7(for-while).py

--- Practice_Sets/Practice7(for-while).py
+++ Practice_Sets/Practice7(for-while).py
@@ -29,3 +29,40 @@ if temp:
         print("it is a prime number")
 else:
         print("it is not a prime number")
+        
+#printing sum of first n natural numbers using formula
+num=int(input("Enter the value of num"))
+sum=0
+while num>0:
+    sum+=num          #sum=sum+num
+    num-=1            #num=num-1
+print(sum)
+
+
+#using for loop
+a=int(input("Enter the number"))
+sum=0
+for s in range(1,a+1):
+    sum+=s
+    s=s+1
+print(sum)
+
+#factorial of a number using for loop
+a=int(input("Enter the number: "))
+fact=1
+for i in range(1,a+1):
+    fact*=i
+print("The factorial of " ,a, "is: ", fact)
+
+#using while while loop
+a=int(input("Enter the Number: "))
+fact=1
+while a>0:
+    fact*=a
+    a-=1
+print("The factorial of number is: ", fact)
+
+
+#using factorial function
+num=int(input("Enter any number ->"))
+print("Factorial of ", num ,"is ->", math.factorial(num))
