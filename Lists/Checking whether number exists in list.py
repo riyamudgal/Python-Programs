c377ccb49afe1b8b05e86ed1fd01b19320c6af76
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-15 22:15:05 +0530

    Create Checking whether number exists in list.py

---
+++ Lists/Checking whether number exists in list.py
@@ -0,0 +1,18 @@
+list1=[1,43,12,89,64,23,56,77]
+
+num=int(input("Enter the number you want to search in list"))
+
+if num in list1:
+    print("Element exits")
+else:
+    print("element do not exits")
+    
+flag=0   
+for i in list1:
+    if i==num:
+        flag=1
+       
+if flag==1:
+     print("Element exits")
+else:
+    print("element do not exits")
