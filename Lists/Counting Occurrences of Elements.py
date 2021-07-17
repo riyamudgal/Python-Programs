f270b131fdeab5c4b975ed038b46fe7a2beae6fc
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-18 00:44:47 +0530

    Update Counting Occurrences of Elements.py

--- Lists/Counting Occurrences of Elements.py
+++ Lists/Counting Occurrences of Elements.py
@@ -1,1 +1,19 @@
+#using Counter(), finding occurrence of particular element
+
+from collections import Counter
+ 
+l = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+ 
+x = 3
+d = Counter(l)
+print(f"{x} has occurred {d[x]} times")
+
+#using Counter(), finding occurrence of all the elements
+from collections import Counter
+
+l = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+ 
+d = Counter(l)
+print(f"occurrence of all ellements is \n {d} ")
+
 
