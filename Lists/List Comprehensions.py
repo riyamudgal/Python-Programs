3172c2de2159bf8a023adeea9e80b9c84a7b9dae
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-23 21:54:41 +0530

    Update List Comprehensions.py

--- Lists/List Comprehensions.py
+++ Lists/List Comprehensions.py
@@ -23,3 +23,14 @@ list1 = [-10,32,-21,65,-4,-76,45,-66,12,93]
 pos_nums= [num for num in list1 if num >= 0]
 print("Positive numbers in the list: ", pos_nums)
 
+#removing even numbers from list
+list1 = [11,21,5,66,17,99,8,10,23,50]
+list1 = [ ele for ele in list1 if elem % 2 != 0]
+ print(list1)
+  
+#removing elements from list1
+list1 = [11,21,5,66,17,99,8,10,23,50]
+non_list=[11,17,99,10]
+list1=[ele for ele in list1 if ele not in non_list]
+print(list1)
+
