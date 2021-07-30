bacf413ffb29b1c38b1386b03d2a367db0a4c259
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-30 12:33:25 +0530

    Update Pattern1.py

--- Patterns/Pattern1.py
+++ Patterns/Pattern1.py
@@ -1,6 +1,17 @@
 #printing a pattern
-n=int(input("enter the no. of rows: "))
+n=int(input("Enter the number of rows: "))
 for i in range(n):
     print(" " *(n-i-1), end=" ")
     print("*"*(2*i+1),end=" ")
     print(" " * (n-i-1))
+    
+    
+'''Output
+Enter the number of rows:
+     *  
+    ***
+   *****
+  *******
+ *********
+ 
+'''
