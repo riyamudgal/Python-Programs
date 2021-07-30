e71a15c76855f8aebe109c8954ecd48483e359f3
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-07-30 12:27:00 +0530

    Update Pattern2.py

--- Patterns/Pattern2.py
+++ Patterns/Pattern2.py
@@ -1,32 +1,30 @@
 #printing pattern
 n=int(input("Enter the number of lines"))
 for i in range(n+1):
-    print("*" *i)
+    print("* " *i)
     
     
 '''Output:
 Enter the number of lines 5
  *
- **
- ***
- ****
- *****
+ * *
+ * * *
+ * * * *
+ * * * * *
  '''
 
-for i in range(0, 5):
+
+n=int(input("Enter the number of lines"))
+for i in range(0,n):
     for j in range(0, i+1):
-        print("*", end="")
-    print("\n")
+        print("* ", end="")
+    print()
     
     
 '''Output
 *
-
-**
-
-***
-
-****
-
-*****
+* *
+* * *
+* * * *
+* * * * *
 '''
