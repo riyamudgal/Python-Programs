0fbde2a96828dd23648ea6e0f0d66f6c17eecac5
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-03 22:31:07 +0530

    Display letter of the word in Pattern

---
+++ Patterns/Pattern10.py
@@ -0,0 +1,26 @@
+strr="LearningFocusing"  
+s=""  
+for i in strr:  
+    x=x+i  
+    print(x)
+    
+    
+'''
+Output
+L
+Le
+Lea
+Lear
+Learn
+Learni
+Learnin
+Learning
+LearningF
+LearningFo
+LearningFoc
+LearningFocu
+LearningFocus
+LearningFocusi
+LearningFocusin
+LeaningFocusing
+'''
