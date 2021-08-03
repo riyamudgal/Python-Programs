dc58b4642fb335f3c07f668cb2714823866d7ab1
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-03 22:53:15 +0530

    Diamond Shaped Pattern

---
+++ Patterns/Pattern11.py
@@ -0,0 +1,39 @@
+rows=int(input("Enter number of rows: "))
+n = 0
+for i in range(1,rows+1):
+    for j in range (1,(rows-i)+1):
+        print(end=" ")
+    while n!=(2*i-1):
+        print("*",end="")
+        n=n+1
+    n=0
+    print()
+ 
+k = 1
+n = 1
+for i in range(1, rows):
+    for j in range (1,k+1):
+        print(end=" ")
+    k=k+1
+    while n<=(2*(rows-i)-1):
+        print("*",end="")
+        n=n+1
+    n=1
+    print()
+    
+    
+'''
+Output
+Enter number of rows: 5
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+    * * * * *
+     * * * *
+      * * *
+       * *
+        *
+        
+'''
