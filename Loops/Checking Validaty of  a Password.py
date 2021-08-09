7bfec91bee548d45a4562e713055fc464de8ae55
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-10 01:37:22 +0530

    Create Checking Validaty of  a Password.py

---
+++ Loops/Checking Validaty of  a Password.py
@@ -0,0 +1,18 @@
+'''
+Validation :
+
+At least 1 letter between [a-z] and 1 letter between [A-Z].
+At least 1 number between [0-9].
+At least 1 character from [$#@].
+Minimum length 6 characters.
+Maximum length 16 characters.
+
+
+Python has a built-in package called re, which can be used to work with Regular Expressions.
+After importing the re module, we can start using regular expressions
+search	Returns a Match object if there is a match anywhere in the string(re.search())
+
+\s -->	Returns a match where the string contains a white space character	--> "\s"
+[]	--> A set of characters	--> "[a-m]"
+
+'''
