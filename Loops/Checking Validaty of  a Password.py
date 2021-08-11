dc8f7c90904e31af8fc2f32048e7109bb43e87bf
Riya Mudgal <75576621+riyamudgal@users.noreply.github.com>
2021-08-11 23:43:05 +0530

    Update Checking Validaty of  a Password.py

--- Loops/Checking Validaty of  a Password.py
+++ Loops/Checking Validaty of  a Password.py
@@ -3,7 +3,7 @@ Validation :
 
 At least 1 letter between [a-z] and 1 letter between [A-Z].
 At least 1 number between [0-9].
-At least 1 character from [$#@].
+At least 1 character from [!@#$&*].
 Minimum length 6 characters.
 Maximum length 16 characters.
 
@@ -16,3 +16,33 @@ search	Returns a Match object if there is a match anywhere in the string(re.sear
 []	--> A set of characters	--> "[a-m]"
 
 '''
+
+import re
+while True:
+    password=input("Enter your password")
+    flag=False
+    if len(password)<6 or len(password)>16:
+        print("Not valid!, Total characters should be between 6 and 16")
+        continue
+    elif not re.search("[A-Z]", password):
+        print("Not valid!, It should contains atleast one letter in upper case [A-Z]")
+        continue
+    elif not re.search("[a-z]", password):
+        print("Not valid!, It should contains atleast on letter in lower case [a-z")
+        continue
+    elif not re.search("[0-9]", password):
+        print("Not valid!, It should contains atleast a number between 0 and 9")
+        continue
+    elif not re.search("[!@#$&*]", password):
+        print("Not valid!, It should contains atleast one special character")
+        continue
+    
+    else:
+        flag=True
+        break
+        
+if flag==True:
+    print("Valid Password")
+    
+    
+#blank space condition has been taken into consideration
